74692783fe64d8a3ab4290907abc0999f7043411
scout911 <qiuliaolin1@163.com>
2021-11-23 16:00:41 +0800

    add shadow for fire

--- src/components/Candle.tsx
+++ src/components/Candle.tsx
@@ -14,7 +14,6 @@ export function CandleEle(props: CandleProps) {
         <div className={`candle ${isBurning ? "candle--burning" : ""}`}>
             <div className="candle__cotton" >
                 <div className="candle__cotton_fire"></div>
-                1
             </div>
             <div className="candle_body">
 
